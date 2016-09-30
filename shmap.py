b15a423012caec8b842e586aed85505c0b575ed5
RobertShala <zyzkill@gmail.com>
2016-09-30 04:07:34 -0400

    Create shmap.py
    Small script to quickly query Shodan for one or multiple IP addresses.

---
+++ shmap.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import shodan
+import sys
+
+
+SHODAN_API_KEY = "ySx58zUQfLSeWKQmmdRKFos4uBofqxKV"
+
+api = shodan.Shodan(SHODAN_API_KEY)
+
+i = 1
+times = len(sys.argv)
+
+while (i != times):
+	
+	try:
+
+		try:
+	        	results = api.host(sys.argv[i])
+	
+
+		        print 'Results found for: %s' % sys.argv[i]
+		        for result in results['data']:
+				print '======================================'
+		                print 'IP: %s | PORT: %s' % (result['ip_str'], result['port'])
+				print '======================================'
+		                print result['data']
+		                print ''
+				i = i + 1
+		except IndexError:
+			print "Scanning finished."
+			quit()
+
+	except shodan.APIError, e:
+	        print 'Error: %s' % e
